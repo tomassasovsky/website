bc7b6ced89369a90c83e5aeac6978b72127ac64b
Tomás Sasovsky <to.sasovsky@gmail.com>
2025-10-08 01:47:25 -0300

    Update URL for Creative Advanced Technologies in clients data

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -5,7 +5,7 @@ export const clients: Client[] = [
   { name: "SportsVisio", logo: "/assets/images/sportsvisio-logo.svg", url: "https://sportsvisio.com/" },
   { name: "CourtAvenue", logo: "/assets/images/courtavenue-logo-on-dark.png", url: "https://www.courtavenue.com/", logoClass: "logo-badge" },
   { name: "VisionBox", logo: "/assets/images/visionbox-ai-logo.png", url: "https://www.visionbox.ca/" },
-  { name: "Creative Advanced Technologies", logo: "/assets/images/cat-logo.webp", url: "https://www.creativeadvancedtech.com/" },
+  { name: "Creative Advanced Technologies", logo: "/assets/images/cat-logo.webp", url: "https://www.creativeadvtech.com/" },
   { name: "It‑TechGroup", logo: "/assets/images/it-techgroup.svg", url: "https://it-techgroup.com/" },
 ];
 
